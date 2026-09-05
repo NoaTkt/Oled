753e5789681a1c3c2b8b4b96549d71994f7fe7a8
NoaTkt <NoaTkt@users.noreply.github.com>
2026-09-05 12:53:00 +0200

    Retire les identifiants Wi-Fi du fichier exemple

--- gps_pieton_esp32/config.example.h
+++ gps_pieton_esp32/config.example.h
@@ -1,7 +1,7 @@
 #pragma once
 
-const char* WIFI_SSID = "iPhoneNoa";
-const char* WIFI_PASS = "iphonenoa";
+const char* WIFI_SSID = "votre_wifi";
+const char* WIFI_PASS = "votre_mot_de_passe_wifi";
 const char* MQTT_HOST = "broker.hivemq.com";
 const int MQTT_PORT = 8883;
 const char* MQTT_TOPIC = "votre-identifiant-gps/gpstopic";
