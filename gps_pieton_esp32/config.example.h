01b349fc2ac5296d478a4c37e9c9b11297bf98a0
NoaTkt <NoaTkt@users.noreply.github.com>
2026-09-05 13:53:04 +0200

    Stabilise les tailles et isole le flux GPS

--- gps_pieton_esp32/config.example.h
+++ gps_pieton_esp32/config.example.h
@@ -4,5 +4,5 @@ const char* WIFI_SSID = "votre_wifi";
 const char* WIFI_PASS = "votre_mot_de_passe_wifi";
 const char* MQTT_HOST = "broker.hivemq.com";
 const int MQTT_PORT = 8883;
-const char* MQTT_TOPIC = "votre-identifiant-gps/gpstopic";
-const char* MQTT_FRAME_TOPIC = "votre-identifiant-gps/frame";
+const char* MQTT_TOPIC = "votre-identifiant-gps-v2/gpstopic";
+const char* MQTT_FRAME_TOPIC = "votre-identifiant-gps-v2/frame";
